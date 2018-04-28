c7894c44228319593fde63e87efb6538971ac589
Sachin Lamba <lamba92sachin@gmail.com>
2018-04-28 18:18:44 +0530

    save list old style

--- src/components/stuffList.js
+++ src/components/stuffList.js
@@ -95,6 +95,79 @@ class stuffList extends React.Component {
       // window.open( "data:text/calendar;charset=utf8," + escape(icsMSG));
     }
 
+        renderTable(item) {
+          let sDate = new Date(item.sales.public.startDateTime),
+              eDate = new Date(item.sales.public.endDateTime),
+              todayDate = new Date();
+          let displaysDate = sDate.getFullYear() + "-" + sDate.getMonth() + "-" + sDate.getDate(),
+              displayeDate = eDate.getFullYear() + "-" + eDate.getMonth() + "-" + eDate.getDate();
+          let left = window.innerWidth/3;
+          let buttonBuy;
+            if(sDate > todayDate){
+              buttonBuy = "sales-not-start";
+            }else if(eDate > todayDate){
+              buttonBuy = "sales-start";
+            }else{
+              buttonBuy = "sales-end";
+            }
+            let event = {
+              title: item.name,
+              description: item.info,
+              location: item._embedded ? item._embedded.venues[0].name : item.place[0].address,
+              startTime: item.sales.public.startDateTime,
+              endTime: item.sales.public.startDateTime
+            };
+            return <div className="events-box" key={item.id}>
+              <div className="event-image col-md-2">
+                <img className="img-circle img-responsive " alt={item.name} src={item.images[2].url} />
+              </div>
+
+                        <div className="event-details" style={{padding: "10px"}}>
+                          <div className="event-name">
+                           <h4 style={{color:"#f7786b"}}>Event Information:</h4>
+                            <label style={{fontSize:"20px"}}>{item.name}</label>
+
+                            <div className="event-venue">
+
+                               <h4 style={{color:"#f7786b", display: "inline-block"}}>Venue:</h4>
+                              {
+                                item._embedded ?
+                                  <label onClick={()=>window.open(item._embedded.venues[0].url, '_target')}
+                                     className="event-address">{"  " + item._embedded.venues[0].name}</label>
+                                  :
+                                  <label className="event-address">{"  " + item.place[0].address}</label>
+                              }
+                              </div>
+
+                              <div className="event-dates">Tickets publically available from {displaysDate} to {displayeDate}</div>
+
+                          </div>
+
+
+
+
+                          <div className="button-space">
+                            <div className={"btn " + buttonBuy} onClick={() => window.open(item.url, '_target')}>Buy</div>
+                            {this.state.savedEvents[item.id] ?
+                              <div className="btn" onClick={this.removeFromEvents.bind(this, item)}>Remove</div>
+                            :
+                              <div className="btn btn-info event-adder" style={{marginTop:"10px",marginBottom:"5px"}}onClick={this.addToEvents.bind(this, item)}>Save to list</div>
+                            }
+                            <AddToCalendar event={event}/>
+                          </div>
+
+                        </div>
+
+
+
+
+                        {/* {item._embedded.venues[0].address.line1 + " > " +
+                        item._embedded.venues[0].city.name + " > " +
+                        item._embedded.venues[0].state.name + " > " +
+                        item._embedded.venues[0].country.countryCode} */}
+                    </div>;
+        }
+
     renderData(item) {
       let sDate = new Date(item.sales.public.startDateTime),
           eDate = new Date(item.sales.public.endDateTime),
@@ -117,60 +190,10 @@ class stuffList extends React.Component {
           startTime: item.sales.public.startDateTime,
           endTime: item.sales.public.startDateTime
         };
-        return
-      <div>
-          <img src={item.images[2].url}/>
-          <p className="legend">{item.name}</p>
-      </div>
-        return <div className="events-box" key={item.id}>
-          <div className="event-image col-md-3`">
-            <img className="img-circle img-responsive " alt={item.name} src={item.images[2].url} />
-          </div>
-
-                    <div className="event-details" style={{padding: "10px"}}>
-                      <div className="event-name">
-                       <h4 style={{color:"#f7786b"}}>Event Information:</h4>
-                        <label style={{fontSize:"20px"}}>{item.name}</label>
-
-                        <div className="event-venue">
-
-                           <h4 style={{color:"#f7786b", display: "inline-block"}}>Venue:</h4>
-                          {
-                            item._embedded ?
-                              <label onClick={()=>window.open(item._embedded.venues[0].url, '_target')}
-                                 className="event-address">{"  " + item._embedded.venues[0].name}</label>
-                              :
-                              <label className="event-address">{"  " + item.place[0].address}</label>
-                          }
-                          </div>
-
-                          <div className="event-dates">Tickets publically available from {displaysDate} to {displayeDate}</div>
-
-                      </div>
-
-
-
-
-                      <div className="button-space">
-                        <div className={"btn " + buttonBuy} onClick={() => window.open(item.url, '_target')}>Buy</div>
-                        {this.state.savedEvents[item.id] ?
-                          <div className="btn" onClick={this.removeFromEvents.bind(this, item)}>Remove</div>
-                        :
-                          <div className="btn btn-info event-adder" style={{marginTop:"10px",marginBottom:"5px"}}onClick={this.addToEvents.bind(this, item)}>Save to list</div>
-                        }
-                        <AddToCalendar event={event}/>
-                      </div>
-
-                    </div>
-
-
-
-
-                    {/* {item._embedded.venues[0].address.line1 + " > " +
-                    item._embedded.venues[0].city.name + " > " +
-                    item._embedded.venues[0].state.name + " > " +
-                    item._embedded.venues[0].country.countryCode} */}
-                </div>;
+        return <div>
+                  <img src={item.images[2].url} />
+                  <p className="legend">{item.name}</p>
+              </div>
     }
     familyOptionChange(changeEvent) {
       this.setState({
@@ -214,7 +237,7 @@ class stuffList extends React.Component {
             )
         }else{
           let data = this.props.stuff._embedded ? this.props.stuff._embedded.events : [];
-          let height = window.innerHeight - 50,
+          let height = window.innerHeight - 100,
               width = window.innerWidth/2 - 20,
              width1 = window.innerWidth/3,
              width2 = window.innerWidth - 120;
@@ -316,14 +339,33 @@ class stuffList extends React.Component {
           }else if(this.state.pageLevel === 1){
             pageDiv.push(
               <div>
-                <div className="">
-                <div showArrows={true}>
+                <div>
+                <div>
                 {
                 data.length ? <Carousel showArrows={true}>{data.map((item, index) => {
+                  let event = {
+                    title: item.name,
+                    description: item.info,
+                    location: item._embedded ? item._embedded.venues[0].name : item.place[0].address,
+                    startTime: item.sales.public.startDateTime,
+                    endTime: item.sales.public.startDateTime
+                  };
                         return (
                           <div>
                               <img src={item.images[2].url}/>
-                              <p className="legend">{item.name}</p>
+                              <p className="legend">{item.name}
+
+                              <div>
+                                <div className="btn btn-primary" onClick={() => window.open(item.url, '_target')}>Buy</div>
+                                {this.state.savedEvents[item.id] ?
+                                  <div className="btn btn-primary" onClick={this.removeFromEvents.bind(this, item)}>Remove</div>
+                                :
+                                  <div className="btn btn-info "onClick={this.addToEvents.bind(this, item)}>Save to list</div>
+                                }
+                                <AddToCalendar event={event}/>
+                              </div>
+
+                              </p>
                           </div>
                         );
                     })
@@ -368,11 +410,11 @@ class stuffList extends React.Component {
             )
           }else if(this.state.pageLevel === 2){
             pageDiv.push(<div>
-                        <div>want to mail saved event calender?</div>
-                        <div style={{flex:"1", height: "96%", overflow: "auto", margin: "5px"}}>{
+                        <div style={{textAlign: "center"}}><h2>want to mail saved event calender?</h2></div>
+                        <div style={{flex:"1", height: height, overflow: "auto", margin: "5px"}}>{
                             Object.keys(this.state.savedEvents).length ? Object.keys(this.state.savedEvents).map((item, index) => {
                                 return (
-                                    this.renderData(this.state.savedEvents[item])
+                                    this.renderTable(this.state.savedEvents[item])
                                 );
                             })
                             :
